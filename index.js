9abbb66164af2d57e0c978ca05d646052ca1beab
Kai Davenport <kaiyadavenport@gmail.com>
2014-06-28 15:47:43 +0000

    return null for null input

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var path = require('path')
 
 module.exports = function(filepath){
+	if(!filepath){
+		return null
+	}
   if(filepath.match(/^\w+:/) || filepath.match(/^\//)){
     return filepath
   }
